9d1ba200b6b18528c1795f5bd9163dd70d1593ff
joean8415 <b02901072@ntu.edu.tw>
2016-12-13 19:30:44 +0800

    Main

--- src/main.py
+++ src/main.py
@@ -1,8 +1,46 @@
 import gym
+import random
+import numpy as np
 
-env = gym.make('Hex9x9-v0')
-env.reset()
+def EpisodeStartStr(num):
+	epi_str = '============\n'
+	epi_str += 'Episode ' + str(num) + '\n'
+	epi_str += '============\n\n'
+	return epi_str
+try:
+	gym = reload(gym)
+	gym.envs.register(
+    	id='MyHex-v0',
+    	entry_point='gym.envs.board_game:HexEnv',
+    	kwargs={
+        	'player_color': 'black',
+        	'opponent': 'random',
+        	'observation_type': 'numpy3c',
+       		'illegal_move_mode': 'lose',
+        	'board_size': 5,
+    	},
+	)
+except Exception as e:
+	print 'Already registered'
 
-for i in range(1000):
+env = gym.make('MyHex-v0')
 
-    env.render()
+for i_episode in range(1):
+	print EpisodeStartStr(i_episode+1)
+
+	initial_gamestate = env.reset()
+	gamestate = initial_gamestate
+
+	for i_step in range(3):
+	    env.render()
+
+
+    	''' Legal Actions '''
+    	legal_actions = env.get_possible_actions(gamestate)
+
+    	''' Random action '''
+    	action = np.random.choice(legal_actions)
+
+    	gamestate, reward, done, info = env.step(action)
+
+    	#env.make_move(gamestate, action, 0)
