d099a58304c123504fe3f2cac3a2d3ce3a9dbec8
joean8415 <b02901072@ntu.edu.tw>
2017-01-10 03:44:43 +0800

    mcts

--- src/agent.py
+++ src/agent.py
@@ -47,16 +47,15 @@ class HumanAgent(Agent):
 
 
 class UCTNode(object):
-    __slots__ = ('value', 'visits')
+	__slots__ = ('value', 'visits')
 
-    def __init__(self, value=0, visits=0):
-        self.value = value
-        self.visits = visits
+	def __init__(self, value=0, visits=0):
+		self.value = value
+		self.visits = visits
 
 class UCTAgent(Agent):
 
 	def __ init__(self, **kwargs):
-		self.history = []
 		self.nodes = {}
 
 		self.calculationTime = float(kwargs.get('time', 30))
@@ -66,116 +65,101 @@ class UCTAgent(Agent):
 		# decrease to prefer actions with known higher win rates.
 		self.C = float(kwargs.get('C', 1.4))
 
-	def update(self, gameState):
-		self.history.append(gameState)
+	def getAction(self, gameState):
+		# Causes the AI to calculate the best action from the
+		# current game state and return it.
+
+		self.max_depth = 0
+		#self.data = {}
+		self.nodes.clear()
+
+		nowPlayer = gameState.nextPlayer
+		legalActions = gameState.getLegalActions()
+
+		# Bail out early if there is no real choice to be made.
+		if len(legalActions) == 0:
+			return None
+		if len(legalActions) == 1:
+			return legalActions[0]
+
+		simulationCount = 0
+		beginTime = time.time()
+		while time.time() - beginTime < self.calculationTime:
+			self.runSimulation()
+			simulationCount += 1
+
+		print('Simulation counts: ', simulationCount)
+		print('Search max depth: ', self.maxDepth)
+		print('Time elapsed: ', time.time() - beginTime)
 
 
-	def getAction(self):
-        # Causes the AI to calculate the best action from the
-        # current game state and return it.
 
-        self.max_depth = 0
-        self.data = {}
-        self.stats.clear()
+		# Store and display the stats for each possible action.
+		#self.data['actions'] = self.calculate_action_values(state, player, legal)
+		#for m in self.data['actions']:
+		#	print self.action_template.format(**m)
 
-        gameState = self.history[-1]
-        nowPlayer = gameState.nextPlayer
-        legalActions = gameState.getLegalActions()
-
-        # Bail out early if there is no real choice to be made.
-        if len(legalActions) == 0:
-        	return None
-        if len(legalActions) == 1:
-        	return legalActions[0]
-
-        simulationCount = 0
-        beginTime = time.time()
-        while time.time() - beginTime < self.calculationTime:
-        	self.runSimulation()
-        	simulationCount += 1
-
-        print('Simulation counts: ', simulationCount)
-        print('Search max depth: ', self.maxDepth)
-        print('Time elapsed: ', time.time() - beginTime)
+		# Pick the action with the highest average value.
+		#return self.board.unpack_action(self.data['actions'][0]['action'])
 
 
-        # Display the number of calls of `run_simulation` and the
-        # time elapsed.
-        self.data.update(games=games, max_depth=self.max_depth,
-                         time=str(time.time() - begin))
-        print self.data['games'], self.data['time']
-        print "Maximum depth searched:", self.max_depth
-
-        # Store and display the stats for each possible action.
-        self.data['actions'] = self.calculate_action_values(state, player, legal)
-        for m in self.data['actions']:
-            print self.action_template.format(**m)
-
-        # Pick the action with the highest average value.
-        return self.board.unpack_action(self.data['actions'][0]['action'])
-
-
-	def runSimulation(self):
+	def runSimulation(self, gameState):
 
 		# Plays out a "random" game from the current position,
-        # then updates the statistics tables with the result.
+		# then updates the statistics tables with the result.
 
-        # A bit of an optimization here, so we have a local
-        # variable lookup instead of an attribute access each loop.
-        
-       	nodes = self.nodes
+		# A bit of an optimization here, so we have a local
+		# variable lookup instead of an attribute access each loop.
+		
+		nodes = self.nodes
 
-        explored = set()
-        history = self.history[:]
-        gameState = history[-1]
-        nowPlayer = gameState.nextPlayer
-        maxActions = self.maxActions
+		explored = set()
+		nowPlayer = gameState.nextPlayer
+		maxActions = self.maxActions
 
-        expand = True
-        for t in range(maxDepth):
-        	legalActions = gameState.getLegalActions()
-        	nextStates = [gameState.getNextState(action) for action in legalActions]
+		expand = True
+		for t in range(maxActions):
+			legalActions = gameState.getLegalActions()
+			nextStates = [gameState.getNextState(action) for action in legalActions]
 
-        	if all( (player, nextState) in nodes for nextState in nextStates):
-        		# UCB1
-        		logSum = log( sum(nodes[ (player, nextState) ].visits for nextState in nextStates) or 1 )
-        		value, state = max(
-        			(( nodes[ (player, nextState) ].value / (nodes[ (player, nextState) ].visits or 1) ) +
-        			self.C * sqrt( logSum / (nodes[ (player, nextState) ].visits or 1)), nextState )
-        			for nextState in nextStates
-        		)
-        	else:
-        		state = choice(nextStates)
+			if all( (player, nextState) in nodes for nextState in nextStates):
+				# UCB1
+				logSum = log( sum(nodes[ (player, nextState) ].visits for nextState in nextStates) or 1 )
+				value, state = max(
+					(( nodes[ (player, nextState) ].value / (nodes[ (player, nextState) ].visits or 1) ) +
+					self.C * sqrt( logSum / (nodes[ (player, nextState) ].visits or 1)), nextState )
+					for nextState in nextStates
+				)
+			else:
+				state = choice(nextStates)
 
-        	history.append(state)
+			if expand and (player, state) not in nodes:
+				expand = False
+				nodes[ (player, state) ] = UCTNode()
+				if t > self.maxDepth:
+					self.maxDepth = t
 
-        	if expand and (player, state) not in nodes:
-        		expand = False
-        		nodes[ (player, state) ] = UCTNode()
-        		if t > self.maxDepth:
-        			self.maxDepth = t
+			explored.add((player, state))
+			if state.isGoalState():
+				break
 
-        	explored.add((player, state))
-        	if state.isGoalState():
-        		break
-
-        # Back-propagation
-        winner = state.getWinner()
-        reward = {}
-        for player in [1,2]:
+		# Back-propagation
+		winner = state.getWinner()
+		reward = {}
+		for player in [1,2]:
 			if winner == 0:
-        		reward[player] = 0.0
-       		elif winner == player:
-        		reward[player] = 1.0
-        	else:
-        		reward[player] = -1.0
+				reward[player] = 0.0
+			elif winner == player:
+				reward[player] = 1.0
+			else:
+				reward[player] = -1.0
 
-        for player, state in explored:
-        	if (player, state) not in nodes:
-        		continue
-        	S = nodes[ (player, state) ]
-        	S.visits += 1
-        	S.value += reward[player]
+		for player, state in explored:
+			if (player, state) not in nodes:
+				continue
+			S = nodes[ (player, state) ]
+			S.visits += 1
+			S.value += reward[player]
 
 
 def NoDeadCellRandomAgent(gameState):
