f33756d6b8829744ff21ff01160f5bcbab696ae3
b02901071 <b02901071@ntu.edu.tw>
2017-01-10 01:02:03 +0800

    hex_mcts.py done & add mcts.py

--- src/hex_mcts.py
+++ src/hex_mcts.py
@@ -1,36 +1,50 @@
-from hex import *
+from hex import HexGameState
 
-class HexMCTS(HexEnv):
+class HexMCTS(HexGameState):
     def display(self, state, action):
-        pass
+        board = str(state)
+        board += "\nPlayed: " + action
+        board += "\nPlayer " + state.nextPlayer + " to move."
+        return board
 
     def pack_state(self, state):
-        pass
+        return state
 
     def pack_action(self, action):
-        pass
+        from ast import literal_eval as make_tuple
+        return make_tuple(action)
 
     def unpack_action(self, action):
-        pass
+        return str(action)
 
     def legal_actions(self, history):
-        pass
+        return history[-1].getLegalActions()
 
     def next_state(self, state, action):
-        pass
+        return state.getNextState(action, state.nextPlayer)
     
     def current_player(self, state):
-        pass
+        return state.nextPlayer
         
     def is_ended(self, history):
-        pass
+        return history[-1].isGoalState()
 
     def win_values(self, history):
-        pass
+        winner = history[-1].getWinner()
+        if winner == 3:
+            return {1: 0.5, 2: 0.5}
+        if not winner:
+            return
+
+        return {winner: 1, 3 - winner: 0}
 
     points_values = win_values
     
     def winner_message(self, winners):
-        pass
+        winners = sorted((v, k) for k, v in winners.iteritems())
+        value, winner = winners[-1]
+        if value == 0.5:
+            return "Stalemate."
+        return "Winner: Player {0}.".format(winner)
 
 
